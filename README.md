f6fc0fd386775a5ee49c7f74575ebb138160c676
Yun Ding <yunding@nvidia.com>
2023-12-19 16:40:39 -0800

    Add test to use local module.

--- README.md
+++ README.md
@@ -1,5 +1,12 @@
 # To reproduce Quarkus bug
 
+`RestClient` from other module. Build error.
+
 ```bash
 ./gradlew :user:test
+```
+
+`RestClient` from same module. Connection refused: localhost/127.0.0.1:80 error as expected.
+```bash
+./gradlew :common:test
 ```
--- common/src/main/java/com/example/ExampleResource.java
+++ common/src/main/java/com/example/ExampleResource.java
@@ -1,16 +1,22 @@
 package com.example;
 
+import java.util.Set;
 import jakarta.ws.rs.GET;
 import jakarta.ws.rs.Path;
 import jakarta.ws.rs.Produces;
 import jakarta.ws.rs.core.MediaType;
+import org.eclipse.microprofile.rest.client.inject.RestClient;
 
 @Path("/hello")
 public class ExampleResource {
 
+  @RestClient
+  ExtensionsService service;
+
   @GET
   @Produces(MediaType.TEXT_PLAIN)
   public String hello() {
+    Set<Extension> extensionSet = service.getById("abc");
     return "Hello from RESTEasy Reactive";
   }
 }
